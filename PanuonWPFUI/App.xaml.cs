c441567423775c72e2e33dce7fdd2f052268b09f
1982840047@qq.com <1982840047@qq.com>
2022-10-30 20:06:48 +0800

    添加项目文件。

---
+++ PanuonWPFUI/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PanuonWPFUI.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace PanuonWPFUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<OnlineClassDashboard>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ PanuonWPFUI/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Panuon.WPF.UI;
+using System.Windows;
+
+namespace PanuonWPFUI.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : WindowX
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
