93e746c77f4c58caf8177595daa7ee5c1f05d6e9
1982840047@qq.com <1982840047@qq.com>
2022-11-14 22:35:49 +0800

    1.PancakeSwap 左侧导航和中间内容完成

---
+++ PanuonWPFUI/ViewModels/PancakeSwaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanuonWPFUI.ViewModels
+{
+   public class PancakeSwaViewModel:BindableBase
+    {
+        public PancakeSwaViewModel()
+        {
+
+        }
+
+    }
+
+    public class Model 
+    { 
+        
+    }
+
+}
--- PanuonWPFUI/Views/PancakeSwap.xaml.cs
+++ PanuonWPFUI/Views/PancakeSwap.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Panuon.WPF.UI;
+﻿using Panuon.WPF;
+using Panuon.WPF.UI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Schema;
 
 namespace PanuonWPFUI.Views
 {
@@ -23,6 +27,77 @@ namespace PanuonWPFUI.Views
         public PancakeSwap()
         {
             InitializeComponent();
+            IcItems.ItemsSource = new ObservableCollection<Model>()
+            {
+                new Model()
+                {
+                    DisplayName="Total Liquidity" ,
+                    Value="$268.3254",
+                    AccentBrush=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ed6f86")),
+                    Icon="\ue945"
+                },
+                new Model()
+                {
+                    DisplayName="Total Liquidity2" ,
+                    Value="$368.3254",
+                    AccentBrush=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#075E9B")),
+                    Icon="\ue946"
+                },
+                new Model()
+                {
+                    DisplayName="Total Liquidity3" ,
+                    Value="$468.3254",
+                    AccentBrush=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7F51C9")),
+                    Icon="\ue947"
+                },
+                new Model()
+                {
+                    DisplayName="Total Liquidity4" ,
+                    Value="$568.3254",
+                    AccentBrush=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#753F67")),
+                    Icon="\ue948"
+                },
+
+            };
+            
+
+        }
+
+        
+    }
+
+    public class Model : NotifyPropertyChangedBase
+    {
+        private string displayName;
+
+        public string DisplayName
+        {
+            get { return displayName; }
+            set { displayName = value; }
+        }
+
+        private Brush accentBrush;
+
+        public Brush AccentBrush
+        {
+            get { return accentBrush; }
+            set { accentBrush = value; }
+        }
+
+        private string icon;
+
+        public string Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+
+        private string _value;
+
+        public string Value
+        {
+            get { return _value; }
+            set { _value = value; }
         }
     }
 }
